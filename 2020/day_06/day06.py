9d5ad2a41be450f33bbeab623939785227c0953c
Luca Fluri <luca.fluri@fhnw.ch>
2024-11-19 21:45:28 +0100

    finish 2020.06

---
+++ 2020/day_06/day06.py
@@ -0,0 +1,38 @@
+import os
+from input_manager import download_and_store_data
+from puzzle_runner import test_with_example, submit_solutions
+import collections
+import math
+
+def solve_part_one(input_data):
+    groups = input_data.split("\n\n")
+    count = 0
+    
+    for group in groups:
+        charSet = set()
+        group = group.replace("\n", "")
+        for char in group:
+            charSet.add(char)
+        count += len(charSet)
+    return count
+        
+
+
+def solve_part_two(input_data):
+    groups = input_data.split("\n\n")
+    count = 0
+    
+    for group in groups:
+        persons = group.split("\n")
+        common_chars = set(persons[0])
+        for person in persons[1:]:
+            common_chars &= set(person)
+        count += len(common_chars)
+    return count
+
+def run():
+    # Use puzzle runner to test with example data
+    test_with_example(2020, 6, solve_part_one, solve_part_two)
+
+    # Use puzzle runner to submit solutions
+    submit_solutions(2020, 6, solve_part_one, solve_part_two)
