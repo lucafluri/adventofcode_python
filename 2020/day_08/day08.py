2991cecd8a1e67adbb5f647801ef7e5ed37e7b70
Luca Fluri <luca.fluri@fhnw.ch>
2024-11-20 18:13:01 +0100

    solve 2020.08.a

--- 2020/day_08/day08.py
+++ 2020/day_08/day08.py
@@ -1,6 +1,20 @@
 from utils.aoc import *
 
+
+
 def solve_part_one(input_data):
+    prog = input_as_lines(input_data)
+    acc = 0
+    pc = 0
+    visited = set()
+    while 0 <= pc < len(prog):
+        if pc in visited: return acc
+        visited.add(pc)
+        op = prog[pc].split(' ')
+        if op[0] == 'acc': acc += int(op[1]); pc += 1
+        elif op[0] == 'jmp': pc += int(op[1])
+        else: pc += 1
+    
     return 0
 
 
@@ -13,5 +27,5 @@ def run():
     test_with_example(2020, 8, solve_part_one, solve_part_two)
 
     # Use puzzle runner to submit solutions
-    #submit_solutions(2020, 8, solve_part_one, solve_part_two)
+    submit_solutions(2020, 8, solve_part_one, solve_part_two)
 
