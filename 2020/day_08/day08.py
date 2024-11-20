18dc139c7dd252dcceff5774881a496c5fe12169
Luca Fluri <luca.fluri@fhnw.ch>
2024-11-20 16:20:44 +0100

    prep 2020.08

---
+++ 2020/day_08/day08.py
@@ -0,0 +1,17 @@
+from utils.aoc import *
+
+def solve_part_one(input_data):
+    return 0
+
+
+def solve_part_two(input_data):
+    return 0
+
+
+def run():
+    # Use puzzle runner to test with example data
+    test_with_example(2020, 8, solve_part_one, solve_part_two)
+
+    # Use puzzle runner to submit solutions
+    #submit_solutions(2020, 8, solve_part_one, solve_part_two)
+
