b9a8f6698bc998542755ca4fc7235bc49a3dc30d
Luca Fluri <luca.fluri@fhnw.ch>
2024-11-28 16:10:39 +0100

    solve 2020.13.a

--- 2020/day_13/day13.py
+++ 2020/day_13/day13.py
@@ -5,6 +5,14 @@ def solve_part_one(input_data):
     earliest, busses = input_data.splitlines()
     earliest = int(earliest)
     busses = [int(b) for b in busses.split(',') if b != 'x']
+    min_wait_time = float('inf')
+    best_bus_id = None
+    for bus_id in busses:
+        wait_time = bus_id - (earliest % bus_id)
+        if wait_time < min_wait_time:
+            min_wait_time = wait_time
+            best_bus_id = bus_id
+    return min_wait_time * best_bus_id
 
 
 def solve_part_two(input_data):
@@ -16,5 +24,4 @@ def run():
     test_with_example(2020, 13, solve_part_one, solve_part_two)
 
     # Use puzzle runner to submit solutions
-    #submit_solutions(2020, 13, solve_part_one, solve_part_two)
-
+    submit_solutions(2020, 13, solve_part_one, solve_part_two)
