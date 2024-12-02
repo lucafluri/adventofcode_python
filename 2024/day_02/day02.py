136486f2614f4eee2541b11bdde26e1da61755e2
Luca Fluri <luca.fluri@fhnw.ch>
2024-12-02 07:42:06 +0100

    solve 2024.02

---
+++ 2024/day_02/day02.py
@@ -0,0 +1,49 @@
+from utils.aoc import *
+
+def parse_input(input_data):
+    data = input_as_lines(input_data)
+    data = [line.split(' ') for line in data]
+    data = [[int(x) for x in line] for line in data]
+    return data
+
+def isValid(line):
+    steps = [1, 2, 3]
+    d = 1 if line[1] - line[0] > 0 else -1
+    for i in range(1, len(line)):
+        d0 = 1 if line[i] - line[i-1] > 0 else -1
+        if abs(line[i] - line[i-1]) not in steps or d0 != d:
+                return False
+    return True
+
+def isValid2(line):
+    steps = [1, 2, 3]
+    d = 1 if line[1] - line[0] > 0 else -1
+    wrongCount = 0
+    for i in range(1, len(line)):
+        d0 = 1 if line[i] - line[i-1] > 0 else -1
+        if abs(line[i] - line[i-1]) not in steps or d0 != d:
+            wrongCount += 1
+            if wrongCount > 1:
+                return False
+    return True
+
+def solve_part_one(input_data):
+    data = parse_input(input_data)
+    valid = list(map(isValid, data))
+    # print(valid)
+    return sum(valid)
+
+
+def solve_part_two(input_data):
+    data = parse_input(input_data)
+    valid = list(map(isValid2, data))
+    # print(valid)
+    return sum(valid)
+
+def run():
+    # Use puzzle runner to test with example data
+    test_with_example(2024, 2, solve_part_one, solve_part_two)
+
+    # Use puzzle runner to submit solutions
+    submit_solutions(2024, 2, solve_part_one, solve_part_two)
+
