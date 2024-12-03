acf0ce4d8717d520602937885dbade8fe25b7b1e
Luca Fluri <luca.fluri@fhnw.ch>
2024-12-03 13:50:29 +0100

    fix day 02

--- 2024/day_02/day02.py
+++ 2024/day_02/day02.py
@@ -10,21 +10,22 @@ def parse_input(input_data):
     data = [[int(x) for x in line] for line in data]
     return data
 
-def isValid(line, part1=True):
+def isValid(line):
     d = 1 if line[1] - line[0] > 0 else -1
-    wrongCount = 0
     for i in range(1, len(line)):
         d0 = 1 if line[i] - line[i-1] > 0 else -1
         step = abs(line[i] - line[i-1])
-        if(step < 1 or step > 3):
+        if(step < 1 or step > 3) or d0 != d:
             return False
-        if d0 != d:
-            if(part1): return False
-            if(wrongCount == 1):
-                return False
-            wrongCount += 1
-        
     return True
+
+def remove_and_test(line):
+    for i in range(len(line)):
+        if isValid(line[:i] + line[i+1:]):
+            return True
+    return False
+
+
 
 def solve_part_one(input_data):
     data = parse_input(input_data)
@@ -34,8 +35,11 @@ def solve_part_one(input_data):
 
 def solve_part_two(input_data):
     data = parse_input(input_data)
-    valid = [isValid(line, False) for line in data]
-    print(valid)
+    valid = [isValid(line) for line in data]
+    
+    for index, line in enumerate(data):
+        if not valid[index] and remove_and_test(line):
+            valid[index] = True
     return sum(valid)
 
 def run():
@@ -43,5 +47,4 @@ def run():
     test_with_example(2024, 2, solve_part_one, solve_part_two, expected_output_part_one, expected_output_part_two)
 
     # Use puzzle runner to submit solutions
-    # submit_solutions(2024, 2, solve_part_one, solve_part_two)
-
+    submit_solutions(2024, 2, solve_part_one, solve_part_two)
