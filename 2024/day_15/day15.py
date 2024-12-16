c2d3802553480487b73c07ea173811b294f15b1a
Luca Fluri <luca.fluri@fhnw.ch>
2024-12-16 15:36:11 +0100

    progress 2024.15

--- 2024/day_15/day15.py
+++ 2024/day_15/day15.py
@@ -4,22 +4,38 @@ from utils.aoc import *
 expected_output_part_one = None 
 expected_output_part_two = None  
 
-def parse_input(input_data):
+def parse_input(input_data, part2=False):
     grid, moves = input_data.split('\n\n')
     moves = moves.replace('\n', '') 
     
-    grid = input_as_grid(grid)
+    if part2:
+        grid = grid.replace('#', '##')
+        grid = grid.replace('O', '[]')
+        grid = grid.replace('.', '..')
+        grid = grid.replace('@', '@.')
     
+    grid = input_as_grid(grid)
+ 
     return grid, moves
+
+# def find_boxes(grid):
+#     boxes = []
+#     for y in range(len(grid)):
+#         for x in range(len(grid[0])):
+#             if grid[y][x] == 'O':
+#                 boxes.append((x, y))
+#             elif grid[y][x] == '[':
+#                 pass
+#     return boxes
+
 
 def get_delta(move='^'):
     if move == '^': return 0, -1
     if move == '>': return 1, 0
     if move == 'v': return 0, 1
     return -1, 0
-    
 
-def moveRobot(grid, pos, move='^'):
+def move_horizontal(grid, pos, move='>'):
     x, y = pos
     dx, dy = get_delta(move)
     
@@ -36,8 +52,6 @@ def moveRobot(grid, pos, move='^'):
             break
         nx, ny = nx + dx, ny + dy
         index += 1
-        
-    # print(blocks, full, index_space)
     
     if not full:
         x = x + dx
@@ -45,23 +59,24 @@ def moveRobot(grid, pos, move='^'):
         
         blocks.insert(0, '@')
         blocks.pop()
-        # print(blocks)
         for i in range(len(blocks)):
             nx, ny = x + dx * i, y + dy * i
             grid[ny][nx] = blocks[i]
-        
-        
-        # for i in range(index_space+1, 1, -1):
-            # nx, ny = x + dx * i, y + dy * i
-            # nnx, nny = x + dx * (i-1), y + dy * (i-1)
-            # print(grid[nny][nnx], grid[ny][nx])
-            # grid[ny][nx] = grid[nny][nnx]
         pos = (x, y)
-        grid[y-dy][x-dx] = '.'
-        # grid[y][x] = '@'
-    
-    
+        grid[y-dy][x-dx] = '.'    
     return grid, pos
+
+def move_vertical(grid, pos, move='v'):
+    x, y = pos
+    
+    return None
+    
+
+def moveRobot(grid, pos, move='^', part2=False):
+    if not part2: return move_horizontal(grid, pos, move)
+    if move in '<>':
+        return move_horizontal(grid, pos, move)
+    return move_vertical(grid, pos, move)
     
     
 def get_score(grid):
@@ -75,8 +90,6 @@ def get_score(grid):
 
 def solve_part_one(input_data):
     grid, moves = parse_input(input_data)
-    # print_grid(grid)
-    # print(moves)
     
     startPos = (0, 0)
     for y in range(len(grid)):
@@ -85,18 +98,27 @@ def solve_part_one(input_data):
                 startPos = (x, y)
     
     for move in moves:
-        grid, startPos = moveRobot(grid, pos=startPos, move=move)
-        # print(f"Move: {move}, Pos: {startPos}")
-    print_grid(grid)
-    
-    
-    
-    
+        grid, startPos = moveRobot(grid, pos=startPos, move=move, part2=False)
+    # print_grid(grid)
+
     return get_score(grid)
 
 
 def solve_part_two(input_data):
-    return None
+    grid, moves = parse_input(input_data, part2=True)
+    print_grid(grid)
+    
+    startPos = (0, 0)
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == '@':
+                startPos = (x, y)
+    
+    for move in moves:
+        grid, startPos = moveRobot(grid, pos=startPos, move=move, part2=True)
+    # print_grid(grid)
+
+    return get_score(grid)
 
 
 def run():
@@ -104,5 +126,5 @@ def run():
     test_with_example(2024, 15, solve_part_one, solve_part_two, expected_output_part_one, expected_output_part_two)
 
     # Use puzzle runner to submit solutions
-    submit_solutions(2024, 15, solve_part_one, solve_part_two)
+    # submit_solutions(2024, 15, solve_part_one, solve_part_two)
 
