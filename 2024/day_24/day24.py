e120dff3ad84e7734f8e1ed1d57bdff7ae4d7a1b
Luca Fluri <luca.fluri@fhnw.ch>
2024-12-24 13:20:30 +0100

    non progress progress

--- 2024/day_24/day24.py
+++ 2024/day_24/day24.py
@@ -2,7 +2,7 @@ from utils.aoc import *
 
 # Define the expected example outputs for part one and part two
 expected_output_part_one = 2024
-expected_output_part_two = None  
+expected_output_part_two = 2024
 
 def parse_input(input_data):
     default, gates = input_data.split('\n\n')
@@ -54,24 +54,61 @@ def solve_part_two(input_data):
     wires, ops = parse_input(input_data)
     
     sorted_wires = sorted(wires.items(), key=lambda x: x[0])
+    
+        # Set all wires starting with 'x' to 1
+    for wire_name in wires:
+        if wire_name.startswith('x'):
+            wires[wire_name] = 1
+
+    # Set all wires starting with 'y' to 1
+    for wire_name in wires:
+        if wire_name.startswith('y'):
+            wires[wire_name] = 0
+    
+    
     x_wires = [wire for wire in sorted_wires if wire[0].startswith('x')]
     y_wires = [wire for wire in sorted_wires if wire[0].startswith('y')]
     
     x_value = get_binary([wire[1] for wire in x_wires])
     y_value = get_binary([wire[1] for wire in y_wires])
     
-    target_z = x_value + y_value
+
+
+    
+    target_z = x_value & y_value
     res = run_instructions(wires, ops)
-    print(x_value, y_value, target_z, res)
+    print(bin(x_value), bin(y_value), bin(target_z), bin(res))
     print(target_z == res)
     # return run_instructions(wires, ops
     
+    print(bin(target_z)[2:].zfill(32))
+    print(bin(res)[2:].zfill(32))
+    print(bin(target_z ^ res)[2:].zfill(32))
+    
+    count = 0
+    A = target_z
+    B = res
+    for i in range(32):
+        # right shift both the numbers by 'i' and
+        # check if the bit at the 0th position is different
+        if ((( A >>  i) & 1) != (( B >>  i) & 1)): 
+             count=count+1
+    print(count)
+
+    count = 0
+    A = ~target_z
+    B = ~res
+    for i in range(32):
+        # right shift both the numbers by 'i' and
+        # check if the bit at the 0th position is different
+        if ((( A >>  i) & 1) != (( B >>  i) & 1)): 
+             count=count+1
+    print(count)
+    
+    # combos = list(it.combinations(sorted_wires, 4)
     
     
-    
-    combos = list(it.combinations(ops, 2))
-    # combo4 = it.combinations(combos, 4)
-    print(len(combos))
+ 
     
     # for c in combos:
     #     c2 = combos.copy()
@@ -88,8 +125,8 @@ def solve_part_two(input_data):
 
 def run():
     # Use puzzle runner to test with example data
-    # test_with_example(2024, 24, solve_part_one, solve_part_two, expected_output_part_one, expected_output_part_two)
+    test_with_example(2024, 24, solve_part_one, solve_part_two, expected_output_part_one, expected_output_part_two)
 
     # Use puzzle runner to submit solutions
-    submit_solutions(2024, 24, solve_part_one, solve_part_two)
+    # submit_solutions(2024, 24, solve_part_one, solve_part_two)
 
