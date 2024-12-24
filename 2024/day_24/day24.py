7eb0bd5b1ecd568b119bf906d18c684a5266cefd
Luca Fluri <dev@lucafluri.ch>
2024-12-24 17:05:02 +0100

    progress

--- 2024/day_24/day24.py
+++ 2024/day_24/day24.py
@@ -2,7 +2,7 @@ from utils.aoc import *
 
 # Define the expected example outputs for part one and part two
 expected_output_part_one = 2024
-expected_output_part_two = 2024
+expected_output_part_two = None  
 
 def parse_input(input_data):
     default, gates = input_data.split('\n\n')
@@ -27,95 +27,110 @@ def get_binary(listWires):
 
 def run_instructions(wires, ops):
     # wires, ops = parse_input(input_data)
-    o = ops.copy()
     
-    while len(o) > 0:
+    while len(ops) > 0:
         
-        for l, op, r, res in o:
+        for l, op, r, res in ops:
             if l in wires and r in wires:
                 if(op == 'AND'): wires[res] = wires[l] & wires[r]
                 if(op == 'OR'): wires[res] = wires[l] | wires[r]
                 if(op == 'XOR'): wires[res] = wires[l] ^ wires[r]
-                o.remove((l, op, r, res))
+                ops.remove((l, op, r, res))
 
     sorted_wires = sorted(wires.items(), key=lambda x: x[0])
     z_wires = [wire for wire in sorted_wires if wire[0].startswith('z')]
-    
-    return get_binary([wire[1] for wire in z_wires])
+    # print(z_wires)
+    return get_binary([wire[1] for wire in z_wires]), z_wires
 
 def solve_part_one(input_data):
     wires, ops = parse_input(input_data)
     
    
-    return run_instructions(wires, ops)
+    return run_instructions(wires, ops)[0]
 
 
 def solve_part_two(input_data):
     wires, ops = parse_input(input_data)
     
     sorted_wires = sorted(wires.items(), key=lambda x: x[0])
-    
-        # Set all wires starting with 'x' to 1
-    for wire_name in wires:
-        if wire_name.startswith('x'):
-            wires[wire_name] = 1
-
-    # Set all wires starting with 'y' to 1
-    for wire_name in wires:
-        if wire_name.startswith('y'):
-            wires[wire_name] = 0
-    
-    
     x_wires = [wire for wire in sorted_wires if wire[0].startswith('x')]
     y_wires = [wire for wire in sorted_wires if wire[0].startswith('y')]
     
     x_value = get_binary([wire[1] for wire in x_wires])
     y_value = get_binary([wire[1] for wire in y_wires])
     
+    """
+    SWAP 1:
+    kgd OR kqf -> z10 <-
+    mwq AND sqr -> kgd
+    x10 AND y10 -> kqf 
+    y10 XOR x10 -> sqr <-
+    
+    444444443333333333222222222211111111110000000000
+    765432109876543210987654321098765432109876543210
+    ------------------------------------------------
+    000110001101110001010001100001011000001000111001
+    000100000010110101010110111111000111111010101111
+    001010010000100110101001011111011111110011101000
+                                         ^
 
 
     
-    target_z = x_value & y_value
-    res = run_instructions(wires, ops)
-    print(bin(x_value), bin(y_value), bin(target_z), bin(res))
-    print(target_z == res)
+    
+    """
+        
+    #replacements
+    
+    replacements = {
+        'z10': 'sqr',
+        'sqr': 'z10',
+        }
+    
+    for o in ops.copy():
+        l, op, r, res = o
+        # print(o)
+        if(res in replacements):
+            res = replacements[res]
+            ops.remove(o)
+            o = (l, op, r, res)
+            ops.append(o)
+        # print(o)
+        
+    
+
+    
+    
+    
+    target_z = x_value + y_value
+    res, z_wires = run_instructions(wires, ops)
+    print(x_value, y_value, target_z, res)
+    # print(target_z == res)
+    # print("Column Numbers:")
+    print("".join(f"{i//10 if i >= 10 else '0'}" for i in range(48))[::-1])
+    print("".join(f"{i%10}" for i in range(48))[::-1])
+    print("".join(f"-" for i in range(48)))
+    
+    # print(bin(x_value)[2:][::-1].zfill(48))
+    # print(bin(y_value)[2:][::-1].zfill(48))
+    # # print(bin(target_z)[2:].zfill(48))
+    
+    # print(bin(res)[2:][::-1].zfill(48))
     # return run_instructions(wires, ops
     
-    print(bin(target_z)[2:].zfill(32))
-    print(bin(res)[2:].zfill(32))
-    print(bin(target_z ^ res)[2:].zfill(32))
-    
-    count = 0
-    A = target_z
-    B = res
-    for i in range(32):
-        # right shift both the numbers by 'i' and
-        # check if the bit at the 0th position is different
-        if ((( A >>  i) & 1) != (( B >>  i) & 1)): 
-             count=count+1
-    print(count)
-
-    count = 0
-    A = ~target_z
-    B = ~res
-    for i in range(32):
-        # right shift both the numbers by 'i' and
-        # check if the bit at the 0th position is different
-        if ((( A >>  i) & 1) != (( B >>  i) & 1)): 
-             count=count+1
-    print(count)
-    
-    # combos = list(it.combinations(sorted_wires, 4)
+    # Show x, y and res value in binary form, stacked over each other
+    # print("Binary Representation (LSB right, MSB left):")
+    print(bin(x_value)[2:].zfill(48))
+    print(bin(y_value)[2:].zfill(48))
+    print(bin(res)[2:].zfill(48))
     
     
- 
     
-    # for c in combos:
-    #     c2 = combos.copy()
-    #     # remove all gates in c from c2
-    #     for g in c:
-    #         c2.remove(g)
-    #     print(len(c2))
+    
+    
+    
+    # combos = list(it.combinations(ops, 2))
+    # combo4 = it.combinations(combos, 4)
+    # print(len(combos))
     # print(len(list(combo4)))
     
     
@@ -128,5 +143,5 @@ def run():
     test_with_example(2024, 24, solve_part_one, solve_part_two, expected_output_part_one, expected_output_part_two)
 
     # Use puzzle runner to submit solutions
-    # submit_solutions(2024, 24, solve_part_one, solve_part_two)
+    submit_solutions(2024, 24, solve_part_one, solve_part_two)
 
